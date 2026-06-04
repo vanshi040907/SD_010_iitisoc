c7bfcea8017a47120255246a72ac667e98aeed76
vanshi040907 <vanshi040907@gmail.com>
2026-06-04 09:47:53 +0530

    initialized backend

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Backend is running successfully!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is live on http://localhost:${PORT}`);
+});
