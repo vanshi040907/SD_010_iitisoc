3e6455b0bc1a02624bacf4a1d2a1371d5492368e
vanshi040907 <vanshi040907@gmail.com>
2026-06-09 10:18:36 +0530

    components added

---
+++ frontend/whiteboard/src/components/Toolbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Toolbar = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Toolbar
---
+++ frontend/whiteboard/src/components/Whiteboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Whiteboard = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Whiteboard
