ccc60344fb1fddef6483bcbc2954a66d022e40cd
vanshi040907 <vanshi040907@gmail.com>
2026-06-10 10:58:18 +0530

    addde session end and reaction bar

--- frontend/whiteboard/src/pages/Workspace.jsx
+++ frontend/whiteboard/src/pages/Workspace.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Toolbar from '../components/Toolbar'
 import TitleCard_download from '../components/TitleCard_download'
 import MemberList from '../components/MemberList'
+import SessionStatus from '../components/SessionStatus'
 const Workspace = () => {
   return (
      <div className="min-h-screen flex items-center justify-center"
@@ -12,6 +13,7 @@ const Workspace = () => {
         <TitleCard_download/>
         <Toolbar/>
         <MemberList />
+        <SessionStatus />
     </div>
   )
 }
